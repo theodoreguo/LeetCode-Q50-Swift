90b1f5d4fdb41cb4689f3053c698ab72ac5a439e
theodoreguo <jsguozitong@gmail.com>
2017-01-20 18:03:46 +0800

    Q13 Completed

--- Chapter 1 - Array:String/13. Longest Palindromic Substring.playground/Contents.swift
+++ Chapter 1 - Array:String/13. Longest Palindromic Substring.playground/Contents.swift
@@ -10,16 +10,100 @@
 
 import Foundation
 
+/*
+ public String longestPalindrome(String s) {
+ int start = 0, end = 0;
+ for (int i = 0; i < s.length(); i++) {
+ int len1 = expandAroundCenter(s, i, i);
+ int len2 = expandAroundCenter(s, i, i + 1);
+ int len = Math.max(len1, len2);
+ if (len > end - start) {
+ start = i - (len - 1) / 2;
+ end = i + len / 2;
+ }
+ }
+ return s.substring(start, end + 1);
+ }
+ private int expandAroundCenter(String s, int left, int right) {
+ int L = left, R = right;
+ while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+ L--;
+ R++; }
+ return R - L - 1;
+ }
+ */
 class Solution {
-    func longestPalindromicString(_ s: String) -> String {
-
+    func longestPalindrome(_ s: String) -> String {
+        let chars = [Character](s.characters)
+        var start = 0, end = 0
+        for i in 0..<chars.count {
+            let len1 = expandAroundCenter(s, i, i)
+            let len2 = expandAroundCenter(s, i, i + 1)
+            let len = max(len1, len2)
+            if len > end - start {
+                start = i - (len - 1) / 2
+                end = i + len / 2
+            }
+        }
+        return String(chars[start ... end])
+    }
+    
+    private func expandAroundCenter(_ s: String, _ left: Int, _ right: Int) -> Int {
+        let chars = [Character](s.characters)
+        var i = left, j = right
+        while i >= 0 && j < chars.count && chars[i] == chars[j] {
+            i -= 1
+            j += 1
+        }
+        return j - i - 1
+    }
+    
+    func longestPalindrome2(_ s: String) -> String {
+        guard s.characters.count > 1 else {
+            return s
+        }
+        
+        var sChars = [Character](s.characters)
+        let len = sChars.count
+        var maxLen = 1
+        var maxStart = 0
+        var isPalin = Array(repeating: Array(repeating: false, count : len), count : len)
+        
+        for i in 0 ... len - 1 {
+            isPalin[i][i] = true
+        }
+        
+        for i in 0 ... len - 2 {
+            if sChars[i] == sChars[i + 1] {
+                isPalin[i][i + 1] = true
+                maxLen = 2
+                maxStart = i
+            }
+        }
+        
+        if len >= 3 {
+            for length in 3 ... len {
+                for i in 0 ... len - length {
+                    if sChars[i] == sChars[i + length - 1] && isPalin[i + 1][i + length - 2] {
+                        isPalin[i][i + length - 1] = true
+                        maxLen = length
+                        maxStart = i
+                    }
+                }
+            }
+        }
+        
+        return String(sChars[maxStart ... maxStart + maxLen - 1])
     }
 }
 
 let obj = Solution()
-let s1 = ""
-let s2 = ""
-let s3 = ""
-//obj.longestPalindromicString(s1)
-//obj.longestPalindromicString(s1)
-//obj.longestPalindromicString(s1)
+let s1 = "abacdgfdcaba"
+let s2 = "aabaa"
+let s3 = "ddgcbaabcclj"
+obj.longestPalindrome(s1)
+obj.longestPalindrome(s2)
+obj.longestPalindrome(s3)
+obj.longestPalindrome2(s1)
+obj.longestPalindrome2(s2)
+obj.longestPalindrome2(s3)
