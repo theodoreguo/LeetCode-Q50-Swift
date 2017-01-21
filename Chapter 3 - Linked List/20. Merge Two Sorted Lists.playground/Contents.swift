bde7d28c3a0a3949dbb2c03ca30e104f34166bc1
theodoreguo <jsguozitong@gmail.com>
2017-01-21 14:39:48 +0800

    Q19 Completed

---
+++ Chapter 3 - Linked List/20. Merge Two Sorted Lists.playground/Contents.swift
@@ -0,0 +1,33 @@
+/**
+ Question:
+ Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ 
+ Link: https://oj.leetcode.com/problems/merge-two-sorted-lists/
+ */
+
+import Foundation
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+    }
+}
+
+//let obj = Solution()
+//let a1 = [9, 9, 9, 9, 9]
+//let a2 = [1, 0, 0]
+//let a3 = [8, 7, 5]
+//obj.mergeTwoLists(a1)
+//obj.mergeTwoLists(a2)
+//obj.mergeTwoLists(a3)
