ef022daebb0856aeb076bf5ebc7210dafa5edff1
theodoreguo <jsguozitong@gmail.com>
2017-01-24 00:15:28 +0800

    Q33/34 Completed

--- Chapter 5 - Bit Manipulation/33. Single Number.playground/Contents.swift
+++ Chapter 5 - Bit Manipulation/33. Single Number.playground/Contents.swift
@@ -16,8 +16,22 @@
 
 import Foundation
 
+/**
+ XOR-ing a number with itself is zero. If we XOR all numbers together, it would effectively cancel out all elements that appear twice leaving us with only the single number. 
+ As the XOR operation is both commutative and associative, the order in how you XOR them does not matter.
+ */
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        
+        var num = 0
+        for x in nums {
+            num ^= x
+        }
+        return num
     }
 }
+
+let obj = Solution()
+let a1 = [2, 3, 6, 4, 4, 5, 3, 6, 2]
+let a2 = [0, 6, 0]
+obj.singleNumber(a1)
+obj.singleNumber(a2)
---
+++ Chapter 6 - Misc/35. Spiral Matrix.playground/Contents.swift
@@ -0,0 +1,27 @@
+/**
+ Question:
+ Given a matrix of m ✕n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ For example, given the following matrix:
+ [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+ ]
+ You should return [1, 2, 3, 6, 9, 8, 7, 4, 5].
+ 
+ Link: https://oj.leetcode.com/problems/spiral-matrix/
+ */
+
+import Foundation
+
+class Solution {
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        
+    }
+}
+
+//let obj = Solution()
+//let a1 = [2, 2, 6, 4, 4, 5, 6, 6, 2, 4]
+//let a2 = [-2, -2, 1, 1, -3, 1, -3, -3, -4, -2]
+//obj.singleNumber(a1)
+//obj.singleNumber(a2)
