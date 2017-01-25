68f49e83620ea9ac1c1adc373fb15922430c9624
theodoreguo <jsguozitong@gmail.com>
2017-01-25 11:48:37 +0800

    Q42 Completed

--- Chapter 8 - Dynamic Programming/42. Climbing Stairs.playground/Contents.swift
+++ Chapter 8 - Dynamic Programming/42. Climbing Stairs.playground/Contents.swift
@@ -11,16 +11,40 @@
 
 import Foundation
 
+/**
+ Define:
+ f(n) = number of ways climbing to the nth step. 
+ To reach to the nth step, only two choices available:
+ 1. Advance one step from the n – 1th step.
+ 2. Advance two steps from the n – 2th step.
+ Therefore, f(n) = f(n – 1) + f(n – 2), which is the exact same recurrence formula defined by the Fibonacci sequence (with different base cases, though).
+ Set base cases f(1) = 1, f(2) = 2.
+ */
 class Solution {
     func climbStairs(_ n: Int) -> Int {
-        
+        var steps = [Int](repeating: 0, count: n + 1)
+        return calcWays(n, &steps)
+    }
+    
+    private func calcWays(_ n: Int, _ steps: inout [Int]) -> Int {
+        if n < 0 {
+            return 0
+        }
+        if n == 0 {
+            return 1
+        }
+        if steps[n] != 0 {
+            return steps[n]
+        }
+        steps[n] = calcWays(n - 1, &steps) + calcWays(n - 2, &steps)
+        return steps[n]
     }
 }
 
-//let obj = Solution()
-//let n1 = 6789
-//let n2 = 467853
-//let n3 = 975389953
-//obj.climbStairs(n1)
-//obj.climbStairs(n2)
-//obj.climbStairs(n3)
+let obj = Solution()
+let n1 = 1
+let n2 = 2
+let n3 = 6
+obj.climbStairs(n1)
+obj.climbStairs(n2)
+obj.climbStairs(n3)
