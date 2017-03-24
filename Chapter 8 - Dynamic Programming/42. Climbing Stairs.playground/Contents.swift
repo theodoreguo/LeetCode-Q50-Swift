3263de4cbb9a03ea87220ac6d4f62b5395f9a503
theodoreguo <jsguozitong@gmail.com>
2017-03-24 10:01:14 +0800

    Optimised comment

--- Chapter 8 - Dynamic Programming/42. Climbing Stairs.playground/Contents.swift
+++ Chapter 8 - Dynamic Programming/42. Climbing Stairs.playground/Contents.swift
@@ -1,8 +1,7 @@
 /**
  Question:
  You are climbing a staircase. It takes n steps to reach to the top.
- Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to
- the top?
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  
  Note: Given n will be a positive integer.
  
