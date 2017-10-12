31dd435432a6a4982ef084bfc29e6a1b8e8c7c44
Zitong Guo <jsguozitong@gmail.com>
2017-10-12 15:15:01 +0800

    Update Q24

--- Chapter 3 - Linked List/24. Copy List with Random Pointer.playground/Contents.swift
+++ Chapter 3 - Linked List/24. Copy List with Random Pointer.playground/Contents.swift
@@ -1,4 +1,5 @@
 /**
+ Question:
  A linked list is given such that each node contains an additional random pointer that could point to any node in the list or null.
  Return a deep copy of the list.
  
